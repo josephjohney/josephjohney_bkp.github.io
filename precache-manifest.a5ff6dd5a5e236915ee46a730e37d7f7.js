551f4c4633b76a6b60d011cadadc3a406804cb74
Kicker <josephjohney@gmail.com>
2020-04-14 15:15:26 +1000

    Updated the deployment

---
+++ precache-manifest.a5ff6dd5a5e236915ee46a730e37d7f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "993e906b31b6d66adf4c06716e828b4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfa6672ef75ec2509ed2",
+    "url": "/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "7fa988ae2e26ddb4f1d4",
+    "url": "/static/css/main.03a0f1af.chunk.css"
+  },
+  {
+    "revision": "cfa6672ef75ec2509ed2",
+    "url": "/static/js/2.d2051edb.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/static/js/2.d2051edb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fa988ae2e26ddb4f1d4",
+    "url": "/static/js/main.5959f55c.chunk.js"
+  },
+  {
+    "revision": "4c15ceb5499f6abee85b",
+    "url": "/static/js/runtime-main.eb07c764.js"
+  }
+]);
